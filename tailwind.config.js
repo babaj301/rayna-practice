6e83230190c8b576bfdea4bc5d979fe713c42185
Babajide Oluwaferanmi <babajideoluwaferanmi301@gmail.com>
2024-10-08 16:18:09 +0100

    Done

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
         'rayna-secondary': '#CC400C',
         'rayna-primary': '#EB5017',
         'rayna-grey': '#667185',
+        'rayna-border': '#D0D5DD',
         'rayna-background': '#F7F9FC',
         'rayna-text': '#101928',
       },
